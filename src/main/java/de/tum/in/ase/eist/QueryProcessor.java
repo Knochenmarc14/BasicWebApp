bf2e3e8fa445f0f38ef6f85fc575e463b21fffb0
marc <ge47pug@mytum.de>
2022-07-13 12:57:57 +0200

    More Req

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,11 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.toLowerCase().contains("what is your name")) {
            return "TestPlayer";
-        } else { // TODO extend the programm here
+        } else if (query.toLowerCase().contains("which of the following numbers is the largest")) {
+            return "20381";
+        } else if (query.toLowerCase().contains("what is 12 plus 19")) {
+            return "31";
+        } else {
             return "";
         }
     }
